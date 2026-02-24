8f131037c8a88b4872c2b241da10c7fc27ab9860
Geet Mantri <geetmantri07@gmail.com>
2026-02-24 19:57:05 +0530

    Experiments done and logging exception tested

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Geet Mantri",
+    author_email="geetmantri07@gmail.com",
+    packages=find_packages()
+)
